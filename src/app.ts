ee7f853bd19233051c07d8edec781dadb4c13ac6
f4rh4d <farhadf4rh4di@gmail.com>
2021-09-26 18:42:21 +0330

    Added Final Step

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const options = { width: 1466, height: 1000 };
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    ignoreHTTPSErrors: true,
+    args: [`--window-size=${options.width},${options.height}`],
+  });
+
   const page = await browser.newPage();
+  await page.setViewport(options);
+
   await page.goto('https://www.skyroom.online/users/login/referrer/736b79726f6f6d2f7374617473');
 
   await page.type('#login_username', 'lingoberry');
@@ -24,15 +33,70 @@ import puppeteer from 'puppeteer';
     visible: true,
   });
 
-  page.click('#btn_select_file > .dropdown-menu > li:nth-child(1) > a');
+  /* Need To Get Type Of File
+      1. zip, pdf, ppt, pptx, odp, png, jpg, jpeg, gif
+      2. mp3, wav
+      3. mp4, webm, ogg
+      4. other
+  */
 
-  await page.screenshot({ path: 'example.png' });
-
-  await page.waitForSelector('#wait', {
+  await page.click('#btn_select_file > .dropdown-menu > li:nth-child(1) > a');
+  await page.waitForSelector('#workspace', {
     visible: true,
   });
 
-  await browser.close();
+  const elementHandle = await page.$('#btn_upload > input[type=file]');
+  await elementHandle?.uploadFile('/home/f4rh4d/Pictures/Screenshot from 2021-08-24 15-02-07.png');
 
-  console.log('Done');
+  //! Didnt Find The Proper El To W8 For
+  // You Can W8 For Input Then Set The Title Too
+  // await page.waitForSelector('#div_title > .col-sm-3 .control-label', {
+  //   visible: true,
+  // });
+  // await page.waitForSelector('#tab_properties > #file_name');
+
+  // await page.$eval('#div_title > input[name=title]', (el: any) => (el.value = 'Adenosine triphosphate'));
+
+  // workspace panel_file tabs
+
+  await page.click('#files > #workspace > #panel_file > #tabs > li:nth-child(2) > a'); // Clicking the link will indirectly cause a navigation
+
+  // page.click('#workspace > #panel_file > #tabs > a[href=#tab_channels');
+  // page.waitForNavigation();
+  //? Selecting The Room
+
+  await page.waitForSelector('#tab_channels', {
+    visible: true,
+  });
+  await page.waitForSelector('#tab_channels > div > #channels', {
+    visible: true,
+  });
+
+  await page.screenshot({ path: 'example.png' });
+
+  await page.evaluate(() =>
+    Array.from(
+      (document as any).querySelector('#tab_channels > div > #channels').children,
+      (checkbox: HTMLElement) => checkbox.children[0],
+    ).forEach((label: Element) => {
+      if ((label as HTMLElement).innerText === 'تست بات اسکای روم') {
+        const el = document.querySelectorAll(
+          `input[type="checkbox"][value="${(label as HTMLElement).children[0].getAttribute('value')}"]`,
+        )[0];
+        el.setAttribute('checked', '');
+      }
+    }),
+  );
+  //? Selecting The Room
+
+  await page.waitForTimeout(500);
+
+  await page.click('#btn_save');
+  // await page.screenshot({ path: 'example.png' });
+
+  // await page.waitForSelector('#wait', {
+  //   visible: true,
+  // });
+
+  // await browser.close();
 })();
